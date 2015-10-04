9ebe65e2c4604988ee8a08b41b1638f8ec8f4337
A. Malone <a_s_malone@hotmail.com>
2015-10-03 20:19:22 -0400

    Created main state machine

--- src/PlayerAI.java
+++ src/PlayerAI.java
@@ -2,41 +2,85 @@ import java.util.*;
 import java.util.Map.Entry;
 import java.awt.Point;
 
-public class PlayerAI extends ClientAI {
+public class PlayerAI extends ClientAI {	
+	
+	//----CONSTANTS
+	//------------------------------------------------------------
+	static final int POWERUP_SCORE = 200;
+	static final int PLAYER_SCORE = 750;
+	static final int TURRET_SCORE = 750;
+	
+	//States
+	static final int FIND_POWER_UPS = 0;
+	static final int TERMINATOR = 1;
+	
+	//----FIELDS
+	//------------------------------------------------------------
+	PotentialField potentialField;
+	int state;
+	
 	public PlayerAI() {
+		potentialField = new PotentialField();
+		state = FIND_POWER_UPS;
 	}
 
 	@Override
 	public Move getMove(Gameboard gameboard, Opponent opponent, Player player)
 			throws NoItemException, MapOutOfBoundsException {
-		// Check for wall tester
-		// System.out.println(checkForWall(gameboard, new Point(1, 0), new
-		// Point(3, 0)) + " == true");
-		// System.out.println(checkForWall(gameboard, new Point(1, 0), new
-		// Point(2, 1)) + " == false");
-		// System.out.println(checkForWall(gameboard, new Point(2, 5), new
-		// Point(2, 2)) + " == true");
-		// System.out.println(checkForWall(gameboard, new Point(0, 3), new
-		// Point(4, 3)) + " == false");
-
-		// int[][] pmap = getPotentialMap(gameboard, opponent, player);
-		// for (int y = 0; y < gameboard.getHeight(); y++) {
-		// for (int x = 0; x < gameboard.getWidth(); x++) {
-		// if (pmap[x][y] >= 9000) {
-		// System.out.print("P");
-		// } else if (pmap[x][y] <= -9000) {
-		// System.out.print("O");
-		// } else if (gameboard.isWallAtTile(x, y)) {
-		// System.out.print("W");
-		// } else {
-		// System.out.print(pmap[x][y]);
-		// }
-		// System.out.print(" ");
-		// }
-		// System.out.println();
-		// }
-
-		// Write your AI here
-		return Move.NONE;
+		
+		//Update the potential field
+		potentialField.updatePotentialMap(gameboard, opponent, player);
+		
+		Move next_move;
+		
+		//Get paths to the different powerups
+		ArrayList<PowerUp> powerUps = gameboard.getPowerUps();
+		if(powerUps.size() > 0){
+			int shortest_length = Integer.MAX_VALUE;
+			Queue<Point> shortest_path = null;
+			
+			for(PowerUp pup : powerUps){
+				try {
+					Queue<Point> path = potentialField.getBestPath(gameboard, player.x, player.y, pup.x, pup.y);
+				} catch (NoPathException e) {
+					
+				}
+			}
+			next_move = shortest_path.peek();
+		}
+		else
+		{
+			next_move = Move.FORWARD;
+		}		
+		return next_move;
+	}
+	
+	private Map<GameObjects, Integer> getObjectives(Gameboard gameboard, Opponent opponent){
+		Map<GameObjects, Integer> map = new HashMap<GameObjects, Integer>();
+		for(PowerUp pup : gameboard.getPowerUps()){
+			map.put(pup, POWERUP_SCORE * objectiveWeight(pup));
+		}
+		map.put(opponent, PLAYER_SCORE * objectiveWeight(opponent));
+		for(Turret tur : gameboard.getTurrets()){
+			map.put(tur, TURRET_SCORE * objectiveWeight(tur));
+		}
+		return map;
+	}
+	
+	private int objectiveWeight(GameObjects obj){
+		switch(state){
+		case FIND_POWER_UPS:
+			if(obj instanceof PowerUp){
+				return 2;
+			}
+			return 1;
+		case TERMINATOR:
+			if(obj instanceof Opponent){
+				return 2;
+			}
+			return 1;
+		default:
+			return 1;
+		}
 	}
 }
