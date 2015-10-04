5b0f5d5f16d2527faddc9c08cab54bff8cd43b33
Wesley <wesley222@gmail.com>
2015-10-04 00:29:43 -0400

    Fixed potential field pathfinding turning on first step.

--- src/PlayerAI.java
+++ src/PlayerAI.java
@@ -9,11 +9,11 @@ public class PlayerAI extends ClientAI {
 	static final int POWERUP_SCORE = 200;
 	static final int PLAYER_SCORE = 750;
 	static final int TURRET_SCORE = 500;
-	
+
 	static final int DANGER_THRESHOLD = 8;
 
-	//----FIELDS
-	//------------------------------------------------------------
+	// ----FIELDS
+	// ------------------------------------------------------------
 	private PotentialField potentialField;
 
 	public PlayerAI() {
@@ -24,7 +24,7 @@ public class PlayerAI extends ClientAI {
 	public Move getMove(Gameboard gameboard, Opponent opponent, Player player)
 			throws NoItemException, MapOutOfBoundsException {
 
-		//----Potential Field Update
+		// ----Potential Field Update
 		potentialField.updatePotentialMap(gameboard, opponent, player);
 		// TODO print potential
 		for (int y = 0; y < gameboard.getHeight(); y++) {
@@ -33,17 +33,15 @@ public class PlayerAI extends ClientAI {
 			}
 			System.out.println();
 		}
-		
-		
-		//----Check Current Danger	
+
+		// ----Check Current Danger
 		int[][] potentialMap = potentialField.getPotentialMap();
 		int currentDanger = potentialMap[player.x][player.y];
-		
-		//----Check for targets
-		//Players
-		
-		
-		//----Plan movement
+
+		// ----Check for targets
+		// Players
+
+		// ----Plan movement
 		Move next_move = Move.FORWARD;
 
 		// Get paths to the different powerups
@@ -54,10 +52,10 @@ public class PlayerAI extends ClientAI {
 			for (Entry<GameObjects, Integer> entry : objectives.entrySet()) {
 				try {
 					GameObjects obj = entry.getKey();
-					Path path = potentialField.getBestPath(gameboard, player.x, player.y, obj.x, obj.y);
-					float roi = (float)(getReward(obj)) / path.cost;
+					Path path = potentialField.getBestPath(gameboard, player, player.x, player.y, obj.x, obj.y);
+					float roi = (float) (getReward(obj)) / path.cost;
 					if (roi > best_roi) {
-						best_roi = roi;	
+						best_roi = roi;
 						shortest_path = path;
 					}
 				} catch (NoPathException e) {
@@ -65,7 +63,11 @@ public class PlayerAI extends ClientAI {
 					continue;
 				}
 			}
-			try {				
+			try {
+				System.out.println(player.x + ", " + player.y);
+				if (shortest_path != null) {
+					System.out.println(shortest_path.path);
+				}
 				next_move = getNextMove(gameboard, player, shortest_path.path.peek());
 			} catch (BadMoveException e) {
 				// TODO Auto-generated catch block
@@ -91,39 +93,42 @@ public class PlayerAI extends ClientAI {
 		if (obj instanceof PowerUp) {
 			return POWERUP_SCORE;
 		} else if (obj instanceof Opponent) {
-			return PLAYER_SCORE;
+			return PLAYER_SCORE / 1000;
 		} else if (obj instanceof Turret) {
-			return TURRET_SCORE;
-		} else {			
+			return TURRET_SCORE / 1000;
+		} else {
 			return 0;
 		}
 	}
 
-	/** 
-	 * Extracts the Move object from the current position inside the 'player' object,
-	 * and the desired position 'pos'
-	 * */
-	private Move getNextMove(Gameboard gameboard, Player player, Point pos) throws BadMoveException {
-
+	/** Finds the direction required to go to an adjacent point */
+	public static Direction getMoveDirection(Gameboard gameboard, int fromX, int fromY, int toX, int toY)
+			throws BadMoveException {
 		// Get the positional differences
-		int dx = PotentialField.wrapDistance(pos.x, player.x, gameboard.getWidth());
-		int dy = PotentialField.wrapDistance(pos.y, player.y, gameboard.getHeight());
-
-		// Calculate the next direction
-		Direction nextDir;
+		int dx = PotentialField.wrapDistanceSigned(fromX, toX, gameboard.getWidth());
+		int dy = PotentialField.wrapDistanceSigned(fromY, toY, gameboard.getHeight());
+		// Calculate the direction
 		if (dx == 0 && dy == 1) {
-			nextDir = Direction.DOWN;
+			return Direction.DOWN;
 		} else if (dx == 0 && dy == -1) {
-			nextDir = Direction.UP;
+			return Direction.UP;
 		} else if (dx == 1 && dy == 0) {
-			nextDir = Direction.RIGHT;
+			return Direction.RIGHT;
 		} else if (dx == -1 && dy == 0) {
-			nextDir = Direction.LEFT;
+			return Direction.LEFT;
 		} else {
 			// Should never happen
-			System.err.println(player.x + ", " + player.y + " -> " + pos.x + ", " + pos.y);
+			System.err.println(fromX + ", " + fromY + " -> " + toX + ", " + toY + " is " + dx + ", " + dy);
 			throw new BadMoveException();
 		}
+	}
+
+	/**
+	 * Extracts the Move object from the current position inside the 'player'
+	 * object, and the desired position 'pos'
+	 */
+	private Move getNextMove(Gameboard gameboard, Player player, Point pos) throws BadMoveException {
+		Direction nextDir = getMoveDirection(gameboard, player.x, player.y, pos.x, pos.y);
 
 		if (nextDir == player.direction) {
 			return Move.FORWARD;
@@ -131,17 +136,17 @@ public class PlayerAI extends ClientAI {
 			return Direction.directionToMovement(nextDir);
 		}
 	}
-	
-	/** 
-	 * Heuristic estimate of combat strength 
-	 * */
-	private int evaluateStrength(Combatant comb){
+
+	/**
+	 * Heuristic estimate of combat strength
+	 */
+	private int evaluateStrength(Combatant comb) {
 		int strength = 0;
-		
+
 		strength += comb.getLaserCount() * 50;
-		strength += comb.getShieldCount() * 50; 
+		strength += comb.getShieldCount() * 50;
 		strength += comb.isShieldActive() ? 100 : 0;
-		
+
 		return strength;
 	}
 }
